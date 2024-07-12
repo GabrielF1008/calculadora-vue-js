c63051e5e61f6683c8f5a14d3c55e9286f92a0a8
Gabriel Oehlerick <gabrielfelipe1009@gmail.com>
2024-07-11 21:26:32 -0300

    parte de adicao adicionada

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,30 @@ import { reactive } from 'vue';
 
 
 const estado = reactive({
-  numero1: 0,
-  numero2: 0,
-  resultado: 0
+  numero1: parseInt(0),
+  numero2: parseInt(0)
 })
 
-function alteraNumero1(evento) {
-  evento => estado.numero1 = evento.target.value
-  console.log(evento.target.value)
-}
-
-function alteraNumero2(evento) {
-  evento => estado.numero2 = evento.target.value
-  console.log(evento.target.value)
-  estado.resultado = estado.numero1 + estado.numero2
+function exibeResultadoMultiplicacao() {
+  return parseInt(estado.numero1) + parseInt(estado.numero2)
 }
 </script>
 
 <template>
-  <form action="">
-    <input type="number" name="numero1" id="numero1" @keyup="alteraNumero1"> <br><br>
-    <input type="number" name="numero2" id="numero2" @keyup="alteraNumero2"> <br><br>
-    <input type="number" name="resultado" id="resultado">
-    {{ estado.resultado }}
+  <div>
     <select>
-      <option value="adicao">Adição</option>
       <option value="subtracao">Subtração</option>
+      <option value="adicao">Adição</option>
       <option value="divisao">Divisão</option>
-      <option value="multiplicacao">Multiplicação</option>
+      <option value="multiplicacao">Multiplicação</option> 
     </select>
-  </form> <br>
+  </div>
+  <form>
+    <br>
+    <input @keyup="evento => estado.numero1 = evento.target.value" type="number" placeholder="Número 1"> <br>
+    <input @keyup="evento => estado.numero2 = evento.target.value" type="number" placeholder="Número 2"> <br>
+    <span> Resultado: {{ exibeResultadoMultiplicacao() }} </span>
+  </form>
 </template>
 
 <style scoped></style>
